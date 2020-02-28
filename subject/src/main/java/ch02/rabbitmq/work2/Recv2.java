578b55b6a58edca62e5ea820e5d22a2a08b34b08
xinyar <guoli.wen@xinyartech.com>
2020-02-28 15:56:13 +0800

    Work模式

---
+++ subject/src/main/java/ch02/rabbitmq/work2/Recv2.java
@@ -0,0 +1,70 @@
+package ch02.rabbitmq.work2;
+
+import ch02.rabbitmq.utils.ConnectionUtil;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * description:
+ *
+ * @author wenguoli
+ * @date 2020/2/28 15:18
+ */
+public class Recv2 {
+    private final static String QUEUE_NAME = "test_queue_work";
+
+    public static void main(String[] argv) throws Exception {
+        // 获取到连接以及mq通道
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = ((Connection) connection).createChannel();
+
+        // 声明队列
+        boolean durable = true;
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+
+        // 同一时刻服务器只会发一条消息给消费者
+        channel.basicQos(1);
+
+        // 定义队列的消费者
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+
+            System.out.println(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } finally {
+                System.out.println(" [x] Done");
+                //开启这行 表示使用手动确认模式
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+        // 监听队列，false表示手动返回完成状态，true表示自动
+        boolean autoAck = false; // acknowledgment is covered below
+        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {
+        });
+
+
+    }
+
+    /**
+     * 执行耗时任务
+     *
+     * @param task
+     */
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}

b6a55f8fc35eaf6528483a443a52386f13e66685
gerry <blog@gerrywen.com>
2020-03-16 22:56:34 +0800

    文件读取完善

---
+++ dahua-design/src/main/java/ch15/Test.java
@@ -0,0 +1,16 @@
+package ch15;
+
+
+/**
+ * program: java-learn->Test
+ * description:
+ * author: gerry
+ * created: 2020-03-16 22:31
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+        String driver = PropertyUtil.getProperty("jdbc.driver");
+        System.out.println(driver);
+    }
+}
